5d19dfc95a397c3e6dfa06c7b358538dc3b3ff6c
PalcevPV <palcevp41@gmail.com>
2026-03-24 21:14:54 +0300

    init

---
+++ Assets/Scripts/InstantiateBulletsShooting.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InstantiateBulletsShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _timeWaitShooting = 2f;
+    [SerializeField] private Transform _target;
+
+    void Start()
+    {
+        StartCoroutine(ShootingWorker());
+    }
+
+    private IEnumerator ShootingWorker()
+    {
+        bool isWork = true;
+
+        while (isWork)
+        {
+            if (_target != null)
+            {
+                Vector3 direction = (_target.position - transform.position).normalized;
+                GameObject bullet = Instantiate(_prefab, transform.position + direction, Quaternion.identity);
+
+                if (bullet.TryGetComponent(out Rigidbody bulletRigidbody))
+                {
+                    bulletRigidbody.transform.up = direction;
+                    bulletRigidbody.linearVelocity = direction * _speed;
+                }
+
+                yield return new WaitForSeconds(_timeWaitShooting);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveThroughPoints.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MoveThroughPoints : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _waypoints;
+    private float _speed = 10f;
+    private int _currentWaypoint = 0;
+
+    private void Update()
+    {
+        if (_waypoints == null || _waypoints.Count == 0)
+            return;
+
+        if (transform.position == _waypoints[_currentWaypoint].position)
+        {
+            _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Count;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentWaypoint].position, _speed * Time.deltaTime);
+    }
+}
