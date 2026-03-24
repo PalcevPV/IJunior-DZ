7754b586040e91094abcf3bec0d03c0afc2997f3
PalcevPV <palcevp41@gmail.com>
2026-03-24 21:26:21 +0300

    refactoring

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class MoveThroughPoints : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private List<Transform> _waypoints;
     private float _speed = 10f;
