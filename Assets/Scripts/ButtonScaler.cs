37fc96da1310dd862adabac063d9f0e27cb1c3d3
PalcevPV <palcevp41@gmail.com>
2026-05-13 18:33:25 +0300

    .
     .

---
+++ Assets/Scripts/ButtonScaler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonScaler : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * 1.1f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * 0.95f;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * 1.1f;
+    }
+}
