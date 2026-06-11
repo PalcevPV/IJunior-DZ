1af25bc1e31e25a24dd51b1857a12fa42013f889
PalcevPV <palcevp41@gmail.com>
2026-06-11 12:58:43 +0300

    optimize

--- Assets/Scripts/UI/HealthView/SmoothHealthBarView.cs
+++ Assets/Scripts/UI/HealthView/SmoothHealthBarView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,15 +6,26 @@ public class SmoothHealthBarView : HealthUI
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private float _speed = 5;
+    private Coroutine _coroutine;
     private float _targetHealth;
-
-    private void Update()
-    {
-        _slider.value = Mathf.MoveTowards(_slider.value, _targetHealth, _speed * Time.deltaTime);
-    }
 
     protected override void UpdateHealthView(float currentHealth, float maxHealth)
     {
         _targetHealth = currentHealth;
+
+        _coroutine = StartCoroutine(AnimateHealthIndicator());
+    }
+
+    private IEnumerator AnimateHealthIndicator()
+    {
+        while (_slider.value != _targetHealth)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, _targetHealth, _speed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
     }
 }
